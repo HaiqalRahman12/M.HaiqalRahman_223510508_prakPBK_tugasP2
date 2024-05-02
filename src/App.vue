d829f3f6f57d884465805323ebda3db5af2fd9b9
HaiqalRahman12 <haiqal04pku@gmail.com>
2024-05-02 22:23:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input v-model="text" placeholder="Type here">
+    <button @click="applyFilter('uppercase')">Uppercase</button>
+    <button @click="applyFilter('lowercase')">Lowercase</button>
+    <p>{{ filteredText }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+const text = ref('')
+
+const filter = ref('');
+
+const filteredText = computed(() => {
+  if (filter.value === 'uppercase') {
+    return text.value.toUpperCase(); 
+  } else if (filter.value === 'lowercase') {
+    return text.value.toLowerCase(); 
+    return text.value; 
+  }
+})
+
+const applyFilter = (selectedFilter) => {
+  filter.value = selectedFilter;
+}
+</script>
